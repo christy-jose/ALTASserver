62f85d7da433941eef7672c63ce185d0b9313fa0
Unknown <christysneha2000@gmail.com>
2018-05-14 00:58:35 +0530

    Connected Server to DB

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -11,6 +11,21 @@
 public class Server {
     public static void Judge (String subid,String testid,String qid) {
         System.out.println("Judging "+subid+" of question "+qid+" of test "+testid);
+        ServerBackEnd SB = new ServerBackEnd();
+        String res = SB.validate(subid, qid);
+        ConnectDB DB =  new ConnectDB();
+        DB.connect();
+        UpdateModule UP = new UpdateModule();
+        UP.setconn(DB.getconn());
+        int score = 0;
+        if(res.equals("Accepted")) {
+            score = 100;
+        }
+        int ret = UP.updateSubmission(res, score, subid);
+        if(ret!=1) {
+            System.out.println("Error in judge or DB");
+        }
+        DB.disconnect();
     }
     
 }
---
+++ src/main/java/UpdateModule.java
@@ -0,0 +1,107 @@
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author christy
+ */
+public class UpdateModule {
+     Connection conn;
+    void UpdateModule(){
+        conn = null;
+    }
+    void setconn(Connection con){
+        conn=con;
+    }
+    int changePassword(String user, String Pass) {
+        PreparedStatement prestmt = null;
+        int ret = 0;
+        try {
+            conn.setAutoCommit(false);
+            String query = "Update  userlist set password = ? where username = ?";
+            //System.out.println(query);
+            prestmt = conn.prepareStatement(query);
+            prestmt.setString(1,Pass);
+            prestmt.setString(2,user);
+            ret = prestmt.executeUpdate();
+            conn.commit();
+            //print(res);
+            System.out.println("Statement excetued Successfully");
+            
+        } catch (SQLException sqle){
+            System.out.println("Statement not excetued");
+            ///*
+            try {
+                conn.rollback();
+                System.out.println("Rollback successful");
+                 
+            } catch (SQLException sqle1){
+                System.out.println("Error");
+                System.out.println(sqle1.getMessage());
+            }
+            //*/
+            System.out.println(sqle.getMessage());
+            
+        } finally {
+            try {
+                conn.setAutoCommit(true);
+                if(prestmt!=null)
+                    prestmt.close();
+            } catch (SQLException sqle){
+                System.out.println("Error");
+                System.out.println(sqle.getMessage());
+            }
+        }
+        return ret; 
+    }
+    int updateSubmission(String status,int score,String id) {
+        PreparedStatement prestmt = null;
+        int ret = 0;
+        try {
+            conn.setAutoCommit(false);
+            String query = "Update  submission set score = ? status = ? where id = ?";
+            //System.out.println(query);
+            prestmt = conn.prepareStatement(query);
+            prestmt.setInt(1,score);
+            prestmt.setString(2,status);
+            prestmt.setString(3,id);
+            ret = prestmt.executeUpdate();
+            conn.commit();
+            //print(res);
+            System.out.println("Statement excetued Successfully");
+            
+        } catch (SQLException sqle){
+            System.out.println("Statement not excetued");
+            ///*
+            try {
+                conn.rollback();
+                System.out.println("Rollback successful");
+                 
+            } catch (SQLException sqle1){
+                System.out.println("Error");
+                System.out.println(sqle1.getMessage());
+            }
+            //*/
+            System.out.println(sqle.getMessage());
+            
+        } finally {
+            try {
+                conn.setAutoCommit(true);
+                if(prestmt!=null)
+                    prestmt.close();
+            } catch (SQLException sqle){
+                System.out.println("Error");
+                System.out.println(sqle.getMessage());
+            }
+        }
+        return ret;
+    }
+}
